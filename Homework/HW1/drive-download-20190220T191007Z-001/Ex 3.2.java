b9309362bacd4b9c1661d8a6014de3113b6c8c6a
Marta Lenartovich <marta.lenartovich@mail.ru>
2019-02-20 22:32:43 +0300

    first commit

---
+++ Homework/HW1/drive-download-20190220T191007Z-001/Ex 3.2.java
@@ -0,0 +1,31 @@
+package core;
+
+public class Task32 {
+	public static void main (String[] args) {
+		//while
+		int i=1;
+		while (i<=15)
+		{
+			System.out.print(i+" ");
+			i++;
+		}
+		System.out.println(" ");
+		
+		//do...while
+		int j=1;
+		do {
+			System.out.print(j+ " ");
+		j++;
+		}
+		while (j<=15);
+		
+		System.out.println(" ");
+		
+		//for
+		int k;
+		for (k=1; k<=15;k++)
+		{
+			System.out.print(k+ " ");
+		}
+	}
+}
---
+++ Homework/HW1/drive-download-20190220T191007Z-001/Ex1- HelloWorld.java
@@ -0,0 +1,23 @@
+package core;
+
+public class HelloWorld {
+	public static void main (String[] args) 
+	{
+	
+	//задание 1	
+	System.out.println ("Hello World!");
+	System.out.println ("Hello Marta!");
+	
+	//задание 2
+	int a=1;
+	double b=223232.3;
+	char c='a';
+	//float d=123.23;
+	System.out.println(a+" "+b+" "+c);
+	
+	//задание 3
+	
+	
+	}
+
+}
